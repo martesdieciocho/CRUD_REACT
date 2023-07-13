18a285d39db760ea7f323c711f690e405baa960a
Alejandro Garcia <agg.9418@gmail.com>
2023-07-12 21:36:11 -0500

    Creación del backend. Conexión a la db y su respectivo modelo

---
+++ Backend/database/db.js
@@ -0,0 +1,9 @@
+import { Sequelize, sequelize } from "sequelize";
+
+//Creación de conexión a BD mediante Sequelize
+const db = new Sequelize('crud_react', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql'
+})
+
+export default db
---
+++ Backend/models/BlogModel.js
@@ -0,0 +1,14 @@
+//Importando la conexión hecha a la BD
+import db from ('../database/db');
+//Importando el DataTypes de Sequelize 
+import {DataTypes} from ('sequelize');
+
+
+//Creando modelo para la tabla de la bd
+const BlogModel = db.define('blogs', {
+    //Se define cada campo de la tabla junto con su tipo de dato
+    title: {type: DataTypes.STRING},
+    content: {type: DataTypes.STRING},
+});
+
+export default BlogModel;
